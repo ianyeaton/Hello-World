8751fb5e39c2484bcd1f8ade7a2db89ce7b5d660
Ian Yeaton <ian.yeaton@gmail.com>
2022-06-28 21:21:02 +0000

    git@github.com:ianyeaton/Notes.git

---
+++ jsonEx.py
@@ -0,0 +1,14 @@
+import json
+
+with open("data_file.json", "r") as info:
+    data = json.load(info)
+
+    print(data["People"][0]["name"])
+
+    data["People"][1]["role"] = "Also the GOAT, I guess"
+
+    with open("next_data_file.json", "w") as s2:
+        json.dump(data, s2)
+
+
+
